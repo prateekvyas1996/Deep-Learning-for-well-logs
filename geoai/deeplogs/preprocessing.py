81db08779ceaaf5d0d95815e7efc6584bf3e4439
Prateek Vyas <VyasPrateek15@stu.upes.ac.in>
2020-08-17 15:12:24 +0530

    updated deeplogs preprocessing
    added documentation in deeplogs preprocessing

--- geoai/deeplogs/preprocessing.py
+++ geoai/deeplogs/preprocessing.py
@@ -6,11 +6,24 @@ import os
 import shutil
 
 def get_file_names(data_dir):
+    '''
+    Input : directory of data, string
+    Output : Return List, names of all files present in directory
+
+    '''
     f_names = os.listdir(data_dir)
     print(f"Files in the directory : {f_names[0:100]} ....")
     return f_names
 
-def read_files_in(f_names, data_dir):
+def read_files_in(data_dir):
+    '''
+    Input : String, directory name where data is present
+    Output : Dataframe, concatenated logs of all logs
+
+    This function will return the concatenated data of all the logs present in the 
+    directory. The missing values are replaced with NaN.
+    '''
+    f_names = get_file_names(data_dir)
     df_list = []
     for fname in f_names:
         f_path = os.path.join(data_dir, fname)
@@ -21,4 +34,3 @@ def read_files_in(f_names, data_dir):
     print(f"Number of files scanned = {len(df_list)}")
     dataframe = pd.concat(df_list,axis=0)
     return dataframe
-    
